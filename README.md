f1f3e7a1d2ecb2f8fe2967f1b81466fa5b9a5eb5
Patricia Carrasco <patri.carrasco@gmail.com>
2024-02-12 15:08:31 +0100

    done

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# 2-2-Project-Image-Processing-Mircoservice-AWS
+# 2-2-Project-Image-Processing-Mircoservice-AWS-
+1. [Project Overview](#schema1)
+2. [Getting Started](#schema2)
+3. [Deploying your system](#schema3)
+4. [REF](#schema4)
+
+
+<hr>
+<a name='schema1'></a>
+
+## 1. Project Overview
+
+**Help Find Missing People**
+You have been hired as a software engineer to develop an application that will help the FBI find missing people. The application will upload images to the FBI cloud database hosted in AWS. This will allow the FBI to run facial recognition software on the images to detect a match. You will be developing a NodeJS server and deploying it on AWS Elastic Beanstalk.
+
+You will build upon the application we've developed during the lessons in this course. You'll complete a REST API endpoint in a backend service that processes incoming image URLs.
+
+
+<hr>
+<a name='schema2'></a>
+
+## 2. Getting Started
+1. Set up node environment
+You'll need to create a new node server. Open a new terminal within the project directory and run:
+
+- Initialize a new project: `npm i`
+- run the development server with `npm run start`
+
+2. Create a new endpoint in the `server.js` file
+Create a new an endpoint in `./src/server.js` which uses query parameter to download an image from a public URL, filter the image, and return the result.
+
+<hr>
+<a name='schema3'></a>
+
+## 3. Deploying your system
+
+Used to `eb init` a new application and `eb create` a new environment to deploy your image-filter service! Don't forget you can use `eb deploy` to push changes.
+
+![eb](./img/eb.png)
+
+
+
+- The url : http://2-2-project-image-processing-mircoservi.us-east-1.elasticbeanstalk.com/
+
+
+
+
+<hr>
+<a name='schemaref'></a>
+
+
+## REF:
+
+
+https://knowledge.udacity.com/questions/994301
---
+++ src/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from '../util/util.js';
+
+(async () => {
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+
+  app.get("/filteredimage", async (req, res) => {
+    const imageUrl = req.query.image_url;
+    console.log("dentro de server.js");
+    console.log(imageUrl);
+    if (!imageUrl) {
+      return res
+        .status(400)
+        .json({ error: "Missing image_url query parameter" });
+    }
+    // if (!imageUrl.match(image_url_regex)) {
+    //   res.status(400).send("image url format is incorrect, example url : https://some_domain(like google.com)/probably_some_more_paths/image_name.png/jpg/gif")
+    // }
+    try {
+      console.log("entra en try");
+      const filteredPath = await filterImageFromURL(imageUrl);
+      console.log("llega del filteredPath", filteredPath);
+      console.log(filteredPath);
+      res.sendFile(filteredPath, {}, async (err) => {
+        if (err) {
+          return res.status(500).json({ error: "Error sending file" });
+        }
+        console.log("delete");
+        await deleteLocalFiles([filteredPath]);
+      });
+    } catch (error) {
+      console.log(error)
+      return res
+        .status(422)
+        .json({ error: "Invalid image_url or unable to process image" });
+    }
+  });
+
+  // app.use((req, res) => {
+  //   res.status(404).json({ error: "Route not found" });
+  // });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
